bbb4cb450ab63a90d52fbd19926df81face38110
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-06-18 13:01:34 -0500

    Update llama.js

--- code/llama.js
+++ code/llama.js
@@ -1,1 +1,11 @@
+var code = document.getElementById("llama").innerHTML;
+var filename = llama.split("<")[1].split(">")[0];
+var output = document.body;
 
+//create title based on LLAMA file name
+var title = document.createElement("title");
+title.innerHTML = filename;
+document.head.appendChild(title);
+
+var format = filename.replace(/f::/gi, "function ").replace(/:BUTTON/gi, "<button").replace(/#/gi, "id='").replace(/\/gi, "'");
+output.innerHTML = format;
