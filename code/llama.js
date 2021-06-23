065841e0e37d7d4f14a616e5d48d8655066f6a2a
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-06-23 18:31:33 -0500

    Update llama.js

--- code/llama.js
+++ code/llama.js
@@ -1,27 +1,8 @@
+//focus textarea
 var txt = document.getElementById("txt");
 txt.focus();
-function runCode() {
-  if (document.getElementById("txt").value.split("cmd")[1] == " --run") {
-    compileCode();
-  }
-  if (document.getElementById("txt").value.split("cmd")[1] == " --download") {
-    alert("LLAMA cannot download yet");
-  }
-  if (document.getElementById("txt").value.split("about")[1] == " --version") {
-    alert("LLAMA\nVersion 1.0");
-  }
-  if (document.getElementById("txt").value.slice(0, 8) == "<file:: ") {
-    document.getElementById("t").innerHTML = document.getElementById("txt").value.split("<file:: ")[1].split(">")[0];
-  }
-}
-function compileCode() {
-  document.getElementById("frame").innerHTML = "";
- if (document.getElementById("txt").value.split("\n").[1].split("::")[0] == "<create[text]") {
-    createText();
- } else if (document.getElementById("txt").value.split("#application:: ")[1].split("#")[0] == "<app:: webwindow::>") {
-    createEmbed();
-  } 
-}
+
+//functions
 function createEmbed() {
   var url = document.createElement("EMBED");
   url.src = document.getElementById("txt").value.split("<create[webwindow]:: ")[1].split("::>")[0];
@@ -36,4 +17,29 @@ function createText() {
   var textLlama = document.createElement("P");
   textLlama.innerHTML = document.getElementById("txt").value.split("<create[text]:: ")[1].split("::>")[0];
   document.getElementById("frame").appendChild(textLlama);
+}
+
+// see which function to do based on code
+function compileCode() {
+  document.getElementById("frame").innerHTML = "";
+ if (document.getElementById("txt").value.split("\n").[1].split("::")[0] == "<create[text]") {
+    createText();
+ } else if (document.getElementById("txt").value.split("#application:: ")[1].split("#")[0] == "<app:: webwindow::>") {
+    createEmbed();
+  } 
+}
+
+function runCode() {
+  if (document.getElementById("txt").value.split("cmd")[1] == " --run") {
+    compileCode();
+  }
+  if (document.getElementById("txt").value.split("cmd")[1] == " --download") {
+    alert("LLAMA cannot download yet");
+  }
+  if (document.getElementById("txt").value.split("about")[1] == " --version") {
+    alert("LLAMA\nVersion 1.0");
+  }
+  if (document.getElementById("txt").value.slice(0, 8) == "<file:: ") {
+    document.getElementById("t").innerHTML = document.getElementById("txt").value.split("<file:: ")[1].split(">")[0];
+  }
 }
