f455f4f9cdb4e43536e9fea77b94968cd07fa697
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-06-23 19:47:42 -0500

    Update llama.js

--- code/llama.js
+++ code/llama.js
@@ -8,6 +8,21 @@ function runCode() {
   if (document.getElementById("txt").value.split("cmd")[1] == " --download") {
     alert("LLAMA cannot download yet");
   }
+  if (document.getElementById("txt").value.split("cmd")[1] == " --upload") {
+    alert("LLAMA cannot upload files yet");
+  }
+  if (document.getElementById("txt").value.split("cmd")[1] == " --print") {
+    window.print();
+  }
+  if (document.getElementById("txt").value.split("cmd")[1] == " --clear") {
+    document.getElementById("txt").value = "";
+  }
+  if (document.getElementById("txt").value.split("cmd")[1] == " --save") {
+    localStorage.setItem("llamacode", document.getElementById("txt").value.split("cmd")[0]);
+  }
+  if (document.getElementById("txt").value.split("cmd")[1] == " --import") {
+    document.getElementById("txt").value = localStorage.getItem("llamacode") + "\ncmd --run";
+  }
   if (document.getElementById("txt").value.split("about")[1] == " --version") {
     alert("LLAMA\nVersion 1.0");
   }
@@ -24,11 +39,27 @@ function compileCode() {
   } else if (document.getElementById("txt").value.split("create(")[1].split(")")[0] == "webwindow") {
     var url = document.createElement("EMBED");
     url.src = document.getElementById("txt").value.split("create(webwindow) \"")[1].split("\"")[0];
-    url.style.position = "absolute";
-    url.style.top = "0px";
-    url.style.left = "0px";
     url.style.width = "99%";
-    url.style.height = "50%";
+    url.style.height = "49%";
     document.getElementById("frame").appendChild(url);
-  } 
+  } else if (document.getElementById("txt").value.split("create(")[1].split(")")[0] == "html") {
+    var textLlama = document.createElement("P");
+    textLlama.innerHTML = document.getElementById("txt").value.split("create(html) \"")[1].split("\";")[0];
+    document.getElementById("frame").appendChild(textLlama);
+  }
+  if (document.getElementById("txt").value.split("create(")[2].split(")")[0] == "text") {
+    var textLlama = document.createElement("P");
+    textLlama.innerHTML = document.getElementById("txt").value.split("create(text) \"")[1].split("\";")[0];
+    document.getElementById("frame").appendChild(textLlama);
+  } else if (document.getElementById("txt").value.split("create(")[2].split(")")[0] == "webwindow") {
+    var url = document.createElement("EMBED");
+    url.src = document.getElementById("txt").value.split("create(webwindow) \"")[2].split("\"")[0];
+    url.style.width = "99%";
+    url.style.height = "49%";
+    document.getElementById("frame").appendChild(url);
+  } else if (document.getElementById("txt").value.split("create(")[2].split(")")[0] == "html") {
+    var textLlama = document.createElement("P");
+    textLlama.innerHTML = document.getElementById("txt").value.split("create(html) \"")[2].split("\";")[0];
+    document.getElementById("frame").appendChild(textLlama);
+  }
 }
